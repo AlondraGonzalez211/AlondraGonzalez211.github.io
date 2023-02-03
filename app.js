90c497abb8c9745115a80c0017f758660bb9d6b3
alondra gonzalez <kirikiribasara1@gmail.com>
2023-02-02 19:17:18 -0800

    added a java script file for animate

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const navLogo = document.querySelector('#navbar_logo')
+const body = document.querySelector('body')
+
+// const mobileMenu
