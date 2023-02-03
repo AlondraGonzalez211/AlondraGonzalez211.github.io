abfe78dacb0f17067dcf7cc73ae393843afc7f26
alondra gonzalez <kirikiribasara1@gmail.com>
2023-02-03 01:43:30 -0800

    added mobile capability

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar_menu')
 const navLogo = document.querySelector('#navbar_logo')
 const body = document.querySelector('body')
-
-// const mobileMenu
+// Display mobile menue
+const mobileMenu = () =>{
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
+}
+menu.addEventListener('click', mobileMenu)
